a8b3c8c7591ac2346488794527fc6201581edf4b
willian1661 <willianderson.sn@gmail.com>
2023-04-13 12:35:53 -0300

    - adjusted header/footer component and their css files

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,34 +1,33 @@
 const Header = () => {
   return (
 
-    <nav className="nav">
-      <div className="nav-container">
+    <nav className="navbar">
+      <div className="Container-1">
         <a href="/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="job" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap hover:text-blue-700">Job</span>
+          <img src="/" className="h-6 mr-3 sm:h-9" alt="job" />
+          <span className="span">
+            Job
+          </span>
         </a>
         <div className="w-full md:block md:w-auto" id="navbar-default">
-          <ul className="nav-ul">
+          <ul className="navbar-list">
             <li>
-              <a href="/" className="nav-main-li" aria-current="page">
+              <a href="/" className="navbar-main-item">
                 Home
               </a>
             </li>
             <li>
-              <a href="/" id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="nav-dropDown-li">Blog
-                <svg className="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd">
-                  </path>
-                </svg>
+              <a href="/" className="navbar-item">
+                Blog
               </a>
             </li>
             <li>
-              <a href="/" className="nav-li">
+              <a href="/" className="navbar-item">
                 About
               </a>
             </li>
             <li>
-              <a href="/" className="nav-li">
+              <a href="/" className="navbar-item">
                 Post a job
               </a>
             </li>
